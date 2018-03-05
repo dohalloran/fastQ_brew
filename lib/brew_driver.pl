6aaf94f4a79caa5cd16c59cf0807054ad621dbc0
ohalloranlab <dohalloran@users.noreply.github.com>
2018-03-05 15:41:07 -0500

    Update brew_driver.pl

--- lib/brew_driver.pl
+++ lib/brew_driver.pl
@@ -11,14 +11,14 @@ $app->run_fastQ_brew();
 =head1 OPTIONS
 
   --i, input file (required)
-  --i2, input file2 only to check demultiplexing 
+  --x, input file2, only to check demultiplexing 
   --plex, checks that two FASTQ files were demultiplexed correctly
   --o, output file (required)    
   --lib, library type  (default is sanger)    
   --dup, remove duplicate reads
   --qf, filter by phred (suggested default=20, min Q score=8 by default)
   --prob, probability that the read contains 0 errors (suggested default=0.5)
-  --lf, filter by read length
+  --lf, filter by read length (suggested default=15)
   --truseq, removes truseq adapters from reads
   --trim_l, trim reads starting at left end
   --trim_r, trim reads starting at left end
