b8a61e62cec78950d7d1d836cec1dad068626648
ohalloranlab <dohalloran@users.noreply.github.com>
2018-03-07 10:50:09 -0500

    Update brew_driver.pl

--- lib/brew_driver.pl
+++ lib/brew_driver.pl
@@ -15,18 +15,15 @@ $app->run_fastQ_brew();
   --plex, checks that two FASTQ files were demultiplexed correctly
   --o, output file (required)    
   --lib, library type  (default is sanger)    
-  --dup, remove duplicate reads
   --qf, filter by phred (suggested default=20, min Q score=8 by default)
   --prob, probability that the read contains 0 errors (suggested default=0.5)
   --lf, filter by read length (suggested default=25)
   --truseq, removes truseq adapters from reads
   --trim_l, trim reads starting at left end
-  --trim_r, trim reads starting at left end
-  --adpt, remove a user supplied adapter      
+  --trim_r, trim reads starting at left end   
   --fasta, convert to fastA format 
   --dna_rna, convert reads to RNA
   --rev_comp, reverse complement reads
-  --no_n, remove non-designated bases from reads
   --help, Print this help
   
 =cut
