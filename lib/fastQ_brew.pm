998f6db051b838e0d4f1cbdb022c9af4e3b7366c
ohalloranlab <dohalloran@users.noreply.github.com>
2025-11-19 11:52:56 -0500

    Update fastQ_brew.pm

--- lib/fastQ_brew.pm
+++ lib/fastQ_brew.pm
@@ -1,101 +1,46 @@
-#
-# module for fastQ_brew
-#
-# Copyright Damien O'Halloran
-#
-# You may distribute this module under the same terms as perl itself
-# POD documentation - main docs before the code
-
-=head1 NAME
-
-fastQ_brew - a modern module for preprocessing of fastQ formatted files
-
-=head1 DESCRIPTION
-
-Provides methods for filtering and trimming reads by length and quality.
-
-=head1 FEEDBACK
-
-damienoh@gwu.edu
-
-=head2 Mailing Lists
-
-User feedback is an integral part of the evolution of this module. Send your comments and suggestions preferably to one of the mailing lists. Your participation is much appreciated.
-
-=head2 Support
-
-Please direct usage questions or support issues to:
-<damienoh@gwu.edu>
-Please include a thorough description of the problem with code and data examples if at all possible.
-
-=head2 Reporting Bugs
-
-Report bugs to the GitHub bug tracking system to help keep track of the bugs and their resolution.  Bug reports can be submitted via the GitHub page:
-
- https://github.com/dohalloran/fastQ_brew/issues
-
-=head1 AUTHORS - Damien OHalloran
-
-Email: damienoh@gwu.edu
-
-=head1 APPENDIX
-
-The rest of the documentation details each method
-
-=cut
-
-# Let the code begin...
-
 package fastQ_brew;
 
+use strict;
+use warnings;
+use feature qw(say);
+
 use Moose;
-with 'MooseX::Getopt', 'MooseX::Getopt::Usage::Role::Man';
-use Modern::Perl;
+
 use fastQ_brew_Utilities;
 use fastQ_brew_Conversions;
 use File::Copy qw(copy);
-use Text::LevenshteinXS qw(distance);
-use if ( $^O eq 'MSWin32' ), 'Win32::Console::ANSI';
-use Term::ANSIColor qw(:constants);
-use List::MoreUtils qw(any first_index);
 use Sys::Hostname;
+use Term::ANSIColor qw(:constants);
 use autodie;
 
 ##################################
-our $VERSION = '2.0';
+our $VERSION = '2.1_core_only';
 ##################################
 
-=head2 fastQ_brew->new_with_options()
+=head1 NAME
 
- Function: Populates the user data into $self 
- Returns : nothing returned
- Args    :
-  --i, input file (required)
-  --x, input file2 only to check demultiplexing 
-  --plex, checks that two FASTQ files were demultiplexed correctly
-  --o, output file (default is filtered.fq)
-  --lib, library type  (default is sanger)       
-  --qf, max. probability for a read to contain errors: suggested p<=50% (must be 1-100)
-  --lf, filter by read length (suggested default =25)
-  --trim_l, trim reads starting at left end
-  --trim_r, trim reads starting at left end
-  --truseq, removes truseq adapters from reads   
-  --fasta, convert to fastA format 
-  --dna_rna, convert reads to RNA
-  --rev_comp, reverse complement reads
-  --help, Print this help
+fastQ_brew - preprocessing of fastQ formatted files (core-only, parallel-safe)
+
+=head1 DESCRIPTION
+
+Filters and trims reads by length and quality, with optional TruSeq adapter
+trimming and simple format conversions. This version is designed to run with
+only core Perl plus Moose and Term::ANSIColor, and to be safe when used from
+an external parallel wrapper.
 
 =cut
 
-##################################
+#----------------------#
+#   Attributes / CLI   #
+#----------------------#
 
-has 'i'        	=> ( is => 'rw', isa => 'Str',  required => 1 );
-has 'x'         => ( is => 'rw', isa => 'Str',  required => 0 );
+has 'i'        	=> ( is => 'rw', isa => 'Str',  required => 1 );  # input FASTQ
+has 'x'         => ( is => 'rw', isa => 'Str',  required => 0 );  # for plex
 has 'plex'      => ( is => 'rw', isa => 'Bool', default  => 0 );
 has 'o'        	=> ( is => 'rw', isa => 'Str',  default  => "filtered.fq" );
 has 'lib'      	=> ( is => 'rw', isa => 'Str',  default  => "sanger" );
-has 'qf'       	=> ( is => 'rw', isa => 'Num',  default  => 0 );
-has 'lf'       	=> ( is => 'rw', isa => 'Int',  default  => 0 );
+has 'qf'       	=> ( is => 'rw', isa => 'Num',  default  => 0 );  # error prob cutoff (%)
+has 'lf'       	=> ( is => 'rw', isa => 'Int',  default  => 0 );  # length filter
 has 'truseq'   	=> ( is => 'rw', isa => 'Bool', default  => 0 );
 has 'trim_l'   	=> ( is => 'rw', isa => 'Int',  default  => 0 );
 has 'trim_r'   	=> ( is => 'rw', isa => 'Int',  default  => 0 );
@@ -103,71 +48,32 @@ has 'fasta'    	=> ( is => 'rw', isa => 'Bool', default  => 0 );
 has 'dna_rna'  	=> ( is => 'rw', isa => 'Bool', default  => 0 );
 has 'rev_comp' 	=> ( is => 'rw', isa => 'Bool', default  => 0 );
 has 'start'    	=> ( is => 'ro', isa => 'Int',  default  => time );
-has help       	=> (
+
+# keep workers for backwards compatibility (not used in this core-only build)
+has 'workers'   => (
+    is            => 'rw',
+    isa           => 'Int',
+    default       => 1,
+    documentation => 'ignored in core-only build; external parallel wrapper is recommended',
+);
+
+# used internally to track the temporary output file
+has 'tmp_file'  => (
+    is        => 'rw',
+    isa       => 'Str',
+    predicate => 'has_tmp_file',
+);
+
+has 'help'     	=> (
     is            	=> 'ro',
     isa           	=> 'Bool',
     default       	=> 0,
-    documentation => qq{view driver script for input flag details}
+    documentation => 'see fastQ_brew.pl for CLI usage',
 );
 
-##################################
-
-=head2 truseq adapters
-
->TruSeq_Universal_Adapter
-AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT
->TruSeq_Adapter_Index_1_6
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACATCACGATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_2
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACCGATGTATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_3
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACTTAGGCATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_4
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACTGACCAATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_5
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACACAGTGATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_6
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACGCCAATATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_7
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACCAGATCATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_8
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACACTTGAATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_9
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACGATCAGATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_10
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACTAGCTTATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_11
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACGGCTACATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_12
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACCTTGTAATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_13
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACAGTCAACAATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_14
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACAGTTCCGTATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_15
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACATGTCAGAATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_16
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACCCGTCCCGATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_18_7
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACGTCCGCACATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_19
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACGTGAAACGATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_20
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACGTGGCCTTATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_21
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACGTTTCGGAATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_22
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACCGTACGTAATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_23
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACGAGTGGATATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_25
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACACTGATATATCTCGTATGCCGTCTTCTGCTTG
->TruSeq_Adapter_Index_27
-GATCGGAAGAGCACACGTCTGAACTCCAGTCACATTCCTTTATCTCGTATGCCGTCTTCTGCTTG
-
-=cut
-
-##################################
+#----------------------#
+#    TruSeq adapters   #
+#----------------------#
 
 my %truseq_seqs = (
     0  => "AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT",
@@ -194,38 +100,130 @@ my %truseq_seqs = (
     21 => "GATCGGAAGAGCACACGTCTGAACTCCAGTCACCGTACGTAATCTCGTATGCCGTCTTCTGCTTG",
     22 => "GATCGGAAGAGCACACGTCTGAACTCCAGTCACGAGTGGATATCTCGTATGCCGTCTTCTGCTTG",
     23 => "GATCGGAAGAGCACACGTCTGAACTCCAGTCACACTGATATATCTCGTATGCCGTCTTCTGCTTG",
-    24 => "GATCGGAAGAGCACACGTCTGAACTCCAGTCACATTCCTTTATCTCGTATGCCGTCTTCTGCTTG"
+    24 => "GATCGGAAGAGCACACGTCTGAACTCCAGTCACATTCCTTTATCTCGTATGCCGTCTTCTGCTTG",
 );
 
-##################################
+#----------------------#
+#   Helper functions   #
+#----------------------#
 
-=head2 run_fastQ_brew()
+# simple Hamming-like distance (length difference counts as mismatches)
+sub _simple_distance {
+    my ( $a, $b ) = @_;
+    my $la = length $a;
+    my $lb = length $b;
+    my $len = $la < $lb ? $la : $lb;
+    my $dist = 0;
 
- Title   : run_fastQ_brew()
- Usage   : $self->run_fastQ_brew(%arg)
- Function: option to filter reads
- Returns : filtered fastQ file
- Args    : $self, %arg
+    for ( my $i = 0; $i < $len; $i++ ) {
+        my $ca = substr( $a, $i, 1 );
+        my $cb = substr( $b, $i, 1 );
+        $dist++ if $ca ne $cb;
+    }
+    $dist += abs( $la - $lb );
+    return $dist;
+}
 
-=cut
+# process one FASTQ record: [header, seq, plus, qual]
+sub _process_record {
+    my ( $self, $rec_ref ) = @_;
+    my @temp = @$rec_ref;
 
-##################################
+    # TruSeq adapter removal
+    if ( $self->{truseq} ) {
+        my @number_matchesR;
+        my @number_matchesL;
+
+        foreach my $key ( sort { $a <=> $b } keys %truseq_seqs ) {
+            my $adapter = $truseq_seqs{$key};
+
+            my $hammingR = substr $temp[1], -length($adapter), length($adapter);
+            push @number_matchesR, _simple_distance( $hammingR, $adapter );
+
+            my $hammingL = substr $temp[1], 0, length($adapter);
+            push @number_matchesL, _simple_distance( $hammingL, $adapter );
+        }
+
+        # right side
+        my $indexR = -1;
+        for ( my $i = 0 ; $i < @number_matchesR ; $i++ ) {
+            if ( $number_matchesR[$i] < 2 ) {
+                $indexR = $i;
+                last;
+            }
+        }
+        if ( $indexR != -1 ) {
+            my $adapter = $truseq_seqs{$indexR};
+            $temp[1]  = substr $temp[1], 0, -length($adapter);
+            $temp[3] = substr $temp[3], 0, -length($adapter);
+        }
+
+        # left side
+        my $indexL = -1;
+        for ( my $i = 0 ; $i < @number_matchesL ; $i++ ) {
+            if ( $number_matchesL[$i] < 2 ) {
+                $indexL = $i;
+                last;
+            }
+        }
+        if ( $indexL != -1 ) {
+            my $adapter = $truseq_seqs{$indexL};
+            $temp[1] = substr $temp[1], length($adapter), length $temp[1];
+            $temp[3] = substr $temp[3], length($adapter), length $temp[3];
+        }
+    }
+
+    # Left trim
+    if ( $self->{trim_l} ) {
+        my $tl = $self->{trim_l};
+        $temp[1] = substr $temp[1], $tl, length $temp[1];
+        $temp[3] = substr $temp[3], $tl, length $temp[3];
+    }
+
+    # Right trim
+    if ( $self->{trim_r} ) {
+        my $tr = $self->{trim_r};
+        $temp[1]  = substr $temp[1], 0, -$tr;
+        $temp[3] = substr $temp[3], 0, -$tr;
+    }
+
+    # Quality filter
+    my $temp_prob = 1;
+    if ( $self->{qf} ) {
+        $temp_prob = _prob_calc( $temp[3], $self->{lib}, $self->{qf} );
+    }
+
+    # Length + prob condition
+    if (
+        (
+               ( $self->{lf} && length( $temp[1] ) > $self->{lf} )
+            || ( !$self->{lf} )
+        )
+        && $temp_prob == 1
+      )
+    {
+        return \@temp;
+    }
+
+    return;    # filtered out
+}
+
+#----------------------#
+#        API          #
+#----------------------#
 
 sub run_fastQ_brew {
     my ( $self, %arg ) = @_;
 
-    my ( $temp, @temp, $count, @number_matchesR, @number_matchesL );
-
-    # log data on sys and infile
     my $inSize = -s $self->{i};
     print BOLD GREEN, "\nPerl Version: \t\t" . $^V;
     print "\nOperating system: \t" . $^O;
     print "\nHostname: \t\t" . hostname;
     print "\nfastQ_brew version: \t" . $VERSION;
-    print "\nInput file size: \t" . $inSize . "KB", RESET;
+    print "\nInput file size: \t" . $inSize . " bytes", RESET;
     print BOLD CYAN, "\n\nFiltering file with fastQ_brew...", RESET;
 
-    #check for file conversion calls or demultiplex check
+    # conversions / demultiplex check
     unless ( $self->{fasta} eq 0 ) {
         _convert_fasta( $self->{i} );
     }
@@ -239,130 +237,74 @@ sub run_fastQ_brew {
         _de_plex( $self->{i}, $self->{x} );
     }
 
-    my $trimmed = "temp_";
+    # core-only build: serial path only (external parallel is recommended)
+    $self->_run_serial(%arg);
+}
 
-    # new outfile resulting from filtering
+sub _run_serial {
+    my ( $self, %arg ) = @_;
+
+    # UNIQUE temp file per process (PID-based)
+    my $trimmed = "temp_$$";
+
+    my @temp;
+    my $count = 0;
+
     open my $fh_out, '>', $trimmed
       or die "Cannot open $trimmed: $!";
 
     open my $fh, '<', $self->{i}
-      or die "Cannot open $self->{in_file}: $!";
+      or die "Cannot open $self->{i}: $!";
 
-    while (<$fh>) {
+    while (1) {
+        my $h = <$fh>;
+        my $s = <$fh>;
+        my $p = <$fh>;
+        my $q = <$fh>;
+        last unless defined $q;   # guard against truncated FASTQ
+
+        chomp( $temp[0] = $h );
+        chomp( $temp[1] = $s );
+        chomp( $temp[2] = $p );
+        chomp( $temp[3] = $q );
+
         $count++;
-        chomp( $temp[0] = $_ );
-        chomp( $temp[1] = <$fh> );
-        chomp( $temp[2] = <$fh> );
-        chomp( $temp[3] = <$fh> );
 
-        my $key;
+        my $out_rec = $self->_process_record( \@temp );
+        next unless $out_rec;
 
-        ##  start by considering adapter removal
-        if ( $self->{truseq} ne 0 ) {
-            @number_matchesR = ();
-            @number_matchesL = ();
-            foreach $key ( sort { $a <=> $b } keys %truseq_seqs ) {
-                my $hammingR = substr $temp[1], -length $truseq_seqs{$key},
-                  length $truseq_seqs{$key};
-                push @number_matchesR,
-                  distance( $hammingR, $truseq_seqs{$key} );
-                my $hammingL = substr $temp[1], 0, length $truseq_seqs{$key};
-                push @number_matchesL,
-                  distance( $hammingL, $truseq_seqs{$key} );
-            }
-
-            if (   any { $_ == 0 } @number_matchesR
-                or any { $_ == 1 } @number_matchesR )
-            {
-                my $index = first_index { $_ < 2 } @number_matchesR;
-                $temp[1]  = substr $temp[1], 0, -length $truseq_seqs{$index};
-                $temp[3] = substr $temp[3], 0, -length $truseq_seqs{$index};
-            }
-            if (   any { $_ == 0 } @number_matchesL
-                or any { $_ == 1 } @number_matchesL )
-            {
-                my $index = first_index { $_ < 2 } @number_matchesL;
-                $temp[1] = substr $temp[1], length $truseq_seqs{$index},
-                  length $temp[1];
-                $temp[3] = substr $temp[3], length $truseq_seqs{$index},
-                  length $temp[3];
-            }
-        }
-
-        # if trim_l option not equal to N, then trim
-        if ( $self->{trim_l} ne 0 ) {
-            $temp[1] = substr $temp[1], $self->{trim_l}, length $temp[1];
-            $temp[3] = substr $temp[3], $self->{trim_l},
-              length $temp[3];
-
-        }
-
-        # if trim_r option not equal to N, then trim
-        if ( $self->{trim_r} ne 0 ) {
-            $temp[1]  = substr $temp[1],  0, -$self->{trim_r};
-            $temp[3] = substr $temp[3], 0, -$self->{trim_r};
-
-        }
-
-        my $temp_prob;
-
-        # get the read error probability score
-        if ( $self->{qf} ne 0 ) {
-            $temp_prob = _prob_calc( $temp[3], $self->{lib}, $self->{qf} );
-        }
-        else {
-            $temp_prob = 1;
-        }
-
-        # conditionals for length
-        if (
-            (
-                   ( length $temp[1] > $self->{lf} && $self->{lf} ne 0 )
-                || ( $self->{lf} eq 0 )
-            )
-            && $temp_prob == 1
-          )
-        {
-            print $fh_out "$temp[0]\n";
-            print $fh_out "$temp[1]\n";
-            print $fh_out "$temp[2]\n";
-            print $fh_out "$temp[3]\n";
-        }
-
+        print $fh_out "$out_rec->[0]\n";
+        print $fh_out "$out_rec->[1]\n";
+        print $fh_out "$out_rec->[2]\n";
+        print $fh_out "$out_rec->[3]\n";
     }
+
     $self->{original_reads} = $count;
     $self->{i}              = $trimmed;
+    $self->{tmp_file}       = $trimmed;
+
     close $fh;
     close $fh_out;
+
     $self->_cleanup(%arg);
 }
 
-##################################
-
-=head2 _cleanup()
-
- Title   : _cleanup()
- Usage   : _cleanup(%arg);
- Function: delete tmp files
- Returns : summary stats
- Args    : %arg
-
-=cut
-
-##################################
-
 sub _cleanup {
     my ( $self, %arg ) = @_;
-    my $count;
+    my $count = 0;
+
     my $clean_out = $self->{o};
     copy $self->{i}, $clean_out;
+
     my $size = -s $clean_out;
 
     open my $fh, '<', $clean_out
       or die "Cannot open $clean_out: $!";
+
     print BOLD YELLOW,
       "\n\nInput number of reads: \t" . $self->{original_reads};
-    print "\nOutput file size: \t" . $size . "KB";
+    print "\nOutput file size: \t" . $size . " bytes";
+
     unless ( $size == 0 ) {
         $count += tr/\n/\n/ while sysread( $fh, $_, 2**16 );
         my $outCount = $count / 4;
@@ -370,25 +312,18 @@ sub _cleanup {
         print "\nReads removed:\t\t"
           . ( ( $self->{original_reads} ) - ($outCount) );
         print "\nPercent removed:\t"
-          . (sprintf "%.6f", (((( ( $self->{original_reads} ) - ($outCount) ))/$self->{original_reads})*100))."%";
+          . ( sprintf "%.6f",
+            ( ( ( $self->{original_reads} - $outCount ) / $self->{original_reads} ) * 100 ) ) . "%";
     }
     my $duration = time - $self->{start};
-    print "\nExecution time:\t\t" . $duration . "secs\n\n", RESET;
+    print "\nExecution time:\t\t" . $duration . " secs\n\n", RESET;
 
-    # delete tmp files
-    if ( -e "temp_" ) {
-        unlink "temp_";
+    # delete this instance's temp file
+    if ( $self->has_tmp_file && -e $self->{tmp_file} ) {
+        unlink $self->{tmp_file};
     }
+
     print BOLD RED, "fastQ_brew is now finished\n\n", RESET;
 }
-
-###################################
-
-=head1 LICENSE AND COPYRIGHT
-
- Copyright (C) 2017 Damien M. O'Halloran
- GNU GENERAL PUBLIC LICENSE
-
-=cut
 
 1;
